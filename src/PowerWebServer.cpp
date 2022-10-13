99300681961a7a8046eaa98ee788fb56f0cc6786
Giovanni Ortu <giovanni.ortu@abinsula.com>
2022-10-13 18:00:13 +0200

    Firts commit

---
+++ src/PowerWebServer.cpp
@@ -0,0 +1,167 @@
+/*
+
+ */
+
+#include "PowerWebServer.h"
+
+
+void loop() 
+{
+  mqtt.loop();
+  server.handleClient();
+ 
+  if (millis() - lastReadingTime > AUTO_READ_DELAY || force_read) 
+  {
+    lastReadingTime = millis();
+    String st = getData();
+    mqtt.publish( topic_send ,st.c_str());
+    force_read = false;
+  }
+#if defined (ARDUINO_ARCH_ESP8266)
+  MDNS.update();
+#endif
+
+  
+}
+
+
+String getData() 
+{
+   Serial.print("Reading data, I'm on ");
+   Serial.println( WiFi.localIP() );
+
+   irms = emon1.calcIrms(1480); // measure current
+   double calcPower = irms*230.0;
+   
+  String st;  
+  st = "{";
+  st += "\"irms\":" + String(irms);
+  st += ",";
+  st += "\"real_power\":" + String(emon1.realPower);
+  st += ",";
+  st += "\"power\":" + String(calcPower);
+  st += "}";
+
+  return st;
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.flush();
+  Serial.println();
+  Serial.println();
+  Serial.println( "------------------------------------------");
+  
+  // Connect to the network
+  WiFi.begin( _SSID, _PASSWORD);            
+  Serial.print("Connecting to "); Serial.print(_SSID); Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED)  // Wait for the Wi-Fi to connect
+  {
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+  }
+
+  Serial.println("Connected!");
+  Serial.print( "My IP address is "); Serial.println( WiFi.localIP() );
+
+  Serial.println();
+
+  Serial.println("Starting mDns...");
+
+#if !defined (ARDUINO_ARCH_ESP8266)
+    mdns_init();
+    mdns_hostname_set(HOSTNAME);
+    mdns_service_add(NULL, "_http", "_tcp", 80, NULL, 0);
+#else
+    if (MDNS.begin( HOSTNAME ) )
+    {
+        MDNS.addService("tcp", "tcp", 80);
+        MDNS.addService("http", "tcp", 80);
+    }
+#endif
+
+     Serial.println("mDns started.");
+
+ 
+    Serial.println(  );
+
+    Serial.println( "Web server setup..." );
+
+    server.on("/", []() {
+        Serial.println( "Serving data." );
+        server.send(200, "application/json", getData() );
+    });
+    
+    
+    server.onNotFound([]() {
+        Serial.println( "HTTP Error!" );
+        server.send(404, "text/html", "<html><head><style>body {background-color: powderblue;} h3 {color: blue;} </style> </head><body><h3>Uri not found <b>"+server.uri()+"</b></h3></body></html>");
+    });
+  
+    server.begin();    
+
+    Serial.print("Webserver up and running on port ");  
+    Serial.println( HTTP_PORT );
+
+    mqtt.setServer( MQTT_BROKER, MQTT_PORT  );
+    mqtt.setCallback( mqttCallback );
+    mqtt.setClient( espClient );
+
+    mqttConnect();
+
+    Serial.println( "Sensor initialization..." );
+    emon1.current(ANALOG_PIN, 111.1);  //pin for current measurement, calibration value
+  
+    //pinMode(8, OUTPUT);   //enable UEXT power supply
+    //digitalWrite(8, LOW);
+
+    Serial.println( "Done!" );
+    Serial.println( "------------------------------------------");
+}
+
+
+bool mqttConnect()
+{  
+  bool retval = false;
+  
+  if (!mqtt.connected())
+  {
+    g_mqttConnectionTimeout = millis();
+
+    Serial.println( "Attempting MQTT connection...");
+
+    if (mqtt.connect( "power_client" ))
+    {
+        Serial.println( "MQTT connected!" );
+        //register to topics
+        Serial.print( "Subscribing to topic " ); Serial.println( topic_recv );
+
+        mqtt.subscribe( topic_recv );
+
+
+        retval = true;
+    }
+    else
+    {
+      delay(500);
+    }    
+  }
+  else
+  {
+    retval = true;
+  }
+  
+  return retval;
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) 
+{
+   payload[ length ]  = '\0';
+   Serial.print( "Received command from MQTT : " );
+   Serial.println( (char*)payload );
+   force_read = true;
+}
---
+++ src/PowerWebServer.h
@@ -0,0 +1,71 @@
+#ifndef POWER_WEB_SERVER_H
+#define POWER_WEB_SERVER_H
+
+
+#include <Arduino.h>
+#include <EmonLib.h>
+#if !defined (ARDUINO_ARCH_ESP8266)
+#include <WiFi.h>
+#include <WebServer.h>
+#include <mDNS.h>
+#else
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#endif
+
+#include <WiFiUdp.h>
+#include <PubSubClient.h>
+
+//Constants and defines
+#define UFFICIO 1
+
+#define AUTO_READ_DELAY 30000
+#define HTTP_PORT 9090
+
+#if !defined (ARDUINO_ARCH_ESP8266)
+#define ANALOG_PIN 34
+#else
+#define ANALOG_PIN A0
+#endif
+
+#ifdef UFFICIO
+const char * _SSID      = "abinsula-28";
+const char * _PASSWORD  = "uff1c10v14l3umb3rt028";
+const char *MQTT_BROKER = "10.0.128.128";
+#else
+const char * _SSID      = "ddwrt";
+const char * _PASSWORD  = "CASA3m1l71a_Rrx1140";
+const char *MQTT_BROKER = "192.168.0.227";
+#endif
+
+const int MQTT_PORT = 1883;
+const char* topic_send = "/home/sensors/powermeter/read";
+const char* topic_recv = "/home/sensors/powermeter/write";
+
+const char* HOSTNAME = "power_meter";
+
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x00, 0xCD };
+
+long lastReadingTime = 0;
+double irms, power;
+int g_mqttConnectionTimeout = 0;
+bool force_read = false;
+
+//Objects
+#if !defined (ARDUINO_ARCH_ESP8266)
+WebServer server(HTTP_PORT);
+#else
+ESP8266WebServer server(HTTP_PORT);
+#endif
+
+WiFiClient espClient;
+EnergyMonitor emon1;
+PubSubClient mqtt;
+
+//Functions
+String getData();
+bool mqttConnect();
+void mqttCallback(char* topic, byte* payload, unsigned int length);
+
+#endif
